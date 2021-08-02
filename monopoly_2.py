b83cb866226da5eb012adca667ee00a1a988955b
KadinP7 <Kadinpar@gmail.com>
2021-08-02 08:09:52 -0400

    Update monopoly_2.py

--- monopoly_2.py
+++ monopoly_2.py
@@ -49,7 +49,7 @@ def pr(x):
 
 properties = {1: "Go", 2: "Mediterranean Avenue", 3: "Community Chest", 4: "Baltic Avenue", 5: "Income Tax", 6: "Jail", 7: "Oriental Avenue", 8: "Chance", 9: "Vermont Avenue", 10: "Conneticut Avenue", 11: "Jail", 12: "St. Charles Place", 13: "Electric Company", 14: "States Avenue", 15: "Virginia Avenue", 16: "Pennsylvania Railroad", 17: "St. James Place", 18: "Community Chest", 19: "Tennessee Avenue", 20: "New York Avenue", 21: "Free Parking", 22: "Kentucky Avenue", 23: "Chance", 24: "Indiana Avenue", 25: "Illinois Avenue", 26: "880 Railroad", 27: "Atlantic Avenue", 28: "Ventor Avenue", 29: "Water Works", 30: "Marvin Gardens", 31: "Go to Jail", 32: "Pacific Avenue", 33: "North Carolina Avenue", 34: "Community Chest", 35: "Pennsylvania Avenue", 36: "Short Line", 37: "Chance", 38: "Park Place", 39: "Luxury Tax", 40: "Boardwalk"}
 
-prices = ["Un", 60, "C", 60, "T", "R", 100, "A", 120, "Un", 140, "E", 140, 160, "R", 180, "C", 180, 200, 0, 220, "A", 220, 240, "R", 260, 260, "E", 280, "J", 300, 300, "C", 320, "R", "A", 350, "T", 400]
+prices = ["A", 60, "B", 60, "C", "D", 100, "E", 120, "F", 140, "G", 140, 160, "H", 180, "I", 180, 200, 0, 220, "J", 220, 240, "K", 260, 260, "L", 280, "M", 300, 300, "N", 320, "O", "P", 350, "Q", 400]
 
 
 deeds = ["Mediterranean Avenue", "Baltic Avenue", "Arctic Avenue", "Masachussetts Avenue", "Oriental Avenue", "Vermont Avenue", "Conneticut Avenue", "Maryland Avenue", "St. Charles Place", "States Avenue", "Virginia Avenue",  "St. James Place", "Tennessee Avenue","New York Avenue",  "Kentucky Avenue", "Indiana Avenue", "Illinois Avenue", "Michigan Avenue",  "Atlantic Avenue", "Ventor Avenue", "Marvin Gardens", "California Avenue",  "Pacific Avenue", "South Carolina Avenue", "North Carolina Avenue", "Pennsylvania Avenue", "Florida Avenue", "Park Place", "Boardwalk", "Reading Railroad", "Pennsylvania Railroad", "B&O Railroad", "Short Line", "Electric Company", "Water Works"]
@@ -63,7 +63,7 @@ chance = {1: "Advance to Go. (Collect $200))", "Advantace to St. Charles Place. 
 
 community = {1: "Advance to Go. ", 2: "Bank error in your favor. Collect $200. ", 3: "Doctor's fees. Pay $50. ", 4: "From sale of stock you get $50.", 5: "Get out of Jail Free. This card may be kept until needed or exchanged. ", 6: "Go to Jail. Go directly to jail.Do not collect $200. ", 7: "Grand Opera Night Collect $50 from every player for opening night seats." 8: "Holiday Fund matures. Collect $100.", 9: "iNCOME TAX REFUND. Collect $20.", 10: "It is your birthday. Collect $10 from every player.", 11: "Life insurance matures - Collect $100. ", 13: "Hospital Fees. Pay $50.", 14: "School Fees. Pay $50.", 15: "Receive $25 consultancy fee.", 16: "You have won second prize in a beauty contest. Collect $10.", 17: "You inherit $100."}
 
-inputBlank = input("Welcome to the classic game of Monopoly! \n\n In this game you, will be playing the digital version of the board game Monopoly.")
+inputBlank = input("Welcome to Monopoly! \n\n In this game you, will be playing the digital version of the board game Monopoly.")
 
 inputPlayers = int(input("How many players are there (2-4: "))
 
